cf18c2d4a303ab7d5eb735feb3459184d681e606
ankitchadha24 <36781509+ankitchadha24@users.noreply.github.com>
2018-12-05 10:12:10 -0500

    Add files via upload

---
+++ Die.java
@@ -0,0 +1,20 @@
+public class Die{
+	private int sides;
+	private int value;
+
+	public Die(){
+		sides=6;
+		value=(int)(Math.random()*6)+1;
+	}
+
+	public int getValue(){
+			return value;
+	}
+
+	public void roll(){
+		value=(int)(Math.random()*sides)+1;
+	}
+	public String toString(){
+		return "Number of sides: "+sides+", Value: "+value;
+	}
+}
